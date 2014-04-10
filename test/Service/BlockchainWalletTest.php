d0d121975b7540143fbf06497a20086c059374bd
Sandro Keil <sandro_keil@web.de>
2014-04-10 22:23:36 +0100

    added tests for blockchain wallet service

--- test/Service/BlockchainWalletTest.php
+++ test/Service/BlockchainWalletTest.php
@@ -32,10 +32,7 @@ class BlockchainWalletTest extends TestCase
      */
     public function testSendWithRequestAddressBalance()
     {
-        $service = $this->getStubForTest();
-        $service->getClient()->getAdapter()->setResponse(
-            file_get_contents(__DIR__ . '/TestAsset/Response/address_balance.txt')
-        );
+        $service = $this->getStubForTest(file_get_contents(__DIR__ . '/TestAsset/Response/address_balance.txt'));
 
         $request = new Request\AddressBalance();
         $request->setAddress('efjsdkfjkwefkwejfkesf');
@@ -45,6 +42,7 @@ class BlockchainWalletTest extends TestCase
 
         $this->assertEquals(0, $response->getBalance());
     }
+
     /**
      * Tests send() with wallet balance request
      *
@@ -55,10 +53,7 @@ class BlockchainWalletTest extends TestCase
      */
     public function testSendWithRequestWalletBalance()
     {
-        $service = $this->getStubForTest();
-        $service->getClient()->getAdapter()->setResponse(
-            file_get_contents(__DIR__ . '/TestAsset/Response/wallet_balance.txt')
-        );
+        $service = $this->getStubForTest(file_get_contents(__DIR__ . '/TestAsset/Response/wallet_balance.txt'));
 
         $request = new Request\WalletBalance();
 
@@ -69,12 +64,60 @@ class BlockchainWalletTest extends TestCase
     }
 
     /**
+     * Tests send() with new address request
+     *
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWallet::send
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWallet::__construct
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWallet::extractData
+     * @group service
+     */
+    public function testSendWithRequestNewAddress()
+    {
+        $service = $this->getStubForTest(file_get_contents(__DIR__ . '/TestAsset/Response/new_address.txt'));
+
+        $request = new Request\NewAddress();
+
+        /* @var $response Response\NewAddress */
+        $response = $service->send($request);
+
+        $this->assertEquals('Order No : 1234', $response->getLabel());
+        $this->assertEquals('18fyqiZzndTxdVo7g9ouRogB4uFj86JJiy', $response->getAddress());
+    }
+
+    /**
+     * Tests send() with list addresses request
+     *
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWallet::send
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWallet::__construct
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWallet::extractData
+     * @group service
+     */
+    public function testSendWithRequestListAddresses()
+    {
+        $service = $this->getStubForTest(file_get_contents(__DIR__ . '/TestAsset/Response/list_addresses.txt'));
+
+        $request = new Request\ListAddresses();
+
+        /* @var $response Response\ListAddresses */
+        $response = $service->send($request);
+
+        $addresses = $response->getAddresses();
+
+        $this->assertCount(3, $addresses);
+        $this->assertArrayHasKey('1Q1AtvCyKhtveGm3187mgNRh5YcukUWjQC', $addresses);
+        $this->assertArrayHasKey('1A8JiWcwvpY7tAopUkSnGuEYHmzGYfZPiq', $addresses);
+        $this->assertArrayHasKey('17p49XUC2fw4Fn53WjZqYAm4APKqhNPEkY', $addresses);
+        $this->assertInstanceOf('\Sake\BlockchainWalletApi\Response\Address', current($addresses));
+    }
+
+    /**
      * Returns stub of test object
      *
+     * @param string $response Http response
      * @param array $methods Methods for test doubles
      * @return \PHPUnit_Framework_MockObject_MockObject
      */
-    protected function getStubForTest(array $methods = null)
+    protected function getStubForTest($response, array $methods = null)
     {
         $stub = $this->getMock(
             '\Sake\BlockchainWalletApi\Service\BlockchainWallet',
@@ -84,6 +127,7 @@ class BlockchainWalletTest extends TestCase
                 new BlockchainWalletOptions()
             )
         );
+        $stub->getClient()->getAdapter()->setResponse($response);
         return $stub;
     }
 }
